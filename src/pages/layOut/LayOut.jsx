fd23536680a01ce24a2b2d207a188a1a23f45082
unknown <ibrahem.youssef2020@hotmail.com>
2023-01-18 14:33:08 +0200

    add loading

--- src/pages/layOut/LayOut.jsx
+++ src/pages/layOut/LayOut.jsx
@@ -1,14 +1,34 @@
+import { useState,useEffect} from 'react'
+import {ThreeDots} from 'react-loader-spinner'
 import Header from './header/Header'
 import Footer from './footer/Footer'
 import { Outlet } from 'react-router-dom'
 
 const LayOut = () => {
-  return (
-    <>
+  const [loading , setLoading] = useState(true)
+
+  useEffect(()=>{
+    setLoading(false)
+  },[loading])
+  const OutPut = loading ? <div className='full-center'>
+  <ThreeDots 
+  height="80" 
+  width="80" 
+  radius="9"
+  color="#4fa94d" 
+  ariaLabel="three-dots-loading"
+  wrapperStyle={{}}
+  wrapperClassName=""
+  visible={true}
+   />
+  </div> : <>
         <Header/>
-            <Outlet/>
+          <Outlet/>
         <Footer/>
-    </>
+        </>
+
+  return (
+    OutPut
   )
 }
 export default LayOut
